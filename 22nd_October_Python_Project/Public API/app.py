5cf25154f7371510dc723ffa06ba4a418628c828
Varun <varungoud56765@gmail.com>
2023-10-28 18:02:41 +0530

    Removed path param

--- 22nd_October_Python_Project/Public API/app.py
+++ 22nd_October_Python_Project/Public API/app.py
@@ -4,8 +4,9 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-@app.route('/<user>')
-def home(user):
+@app.route('/')
+def home():
+    user = "sudh9931"
     print(user)
     response = requests.get("https://api.github.com/users/" + user)
     data = response.json()
