6a704d7ceea6f496bf7bd63360898259d98bf733
Varun <varungoud56765@gmail.com>
2023-10-28 17:45:08 +0530

    Public API project

---
+++ 22nd_October_Python_Project/Public API/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/<user>')
+def home(user):
+    print(user)
+    response = requests.get("https://api.github.com/users/" + user)
+    data = response.json()
+    return render_template("index.html", data=data)
+
+if __name__ == "__main__":
+    app.run()
